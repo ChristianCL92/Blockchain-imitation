0193c020f4a91b23d78b385a1273adf08b72aa2d
Christian Lopez <chrisdev92@outlook.com>
2024-05-03 14:20:23 +0200

    testing createblock with data

--- controllers/blockchain-controller.mjs
+++ controllers/blockchain-controller.mjs
@@ -1,3 +1,24 @@
+ import ResponseModel from "../utilities/ResponseModel.mjs" 
+import blockchain from "../startBlockchain.mjs"
+
 export const getBlockchain = (req, res, next) => {
-    res.status(200).json({success: true, data:`hello you have a blockchain`})
+res.status(200).json(new ResponseModel({statusCode: 200, data: blockchain}))
+}
+
+export const createBlock = (req, res, next) => {
+  const data = req.body;
+     const previousBlockHash = 0;
+    const currentBlockHash = 0;
+    const timestamp = Date.now(); 
+   const block = blockchain.createBlock(
+    timestamp,
+    previousBlockHash,
+    currentBlockHash,
+    data
+  ); 
+
+console.log(timestamp);
+
+  res.status(201).json(new ResponseModel({ statusCode: 201, data: block }));
+  console.log(block);
 }
