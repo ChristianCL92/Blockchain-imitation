46307e4c36b82b314aef27aa6cc1eb49f8b4a197
Christian Lopez <chrisdev92@outlook.com>
2024-05-06 20:56:06 +0200

    included errorhandling for attempt to add empty block

--- controllers/blockchain-controller.mjs
+++ controllers/blockchain-controller.mjs
@@ -3,18 +3,19 @@ import blockchain from "../startBlockchain.mjs"
   import {
     writeToFile,
   } from '../utilities/fileHandler.mjs';  
+import { ErrorResponse } from "../utilities/ErrorResponseModel.mjs";
+
 
   const folder = "data";
 const file = "blockchain.json";  
 
 
-export const getBlockchain = (req, res, next) => {  
-  console.log(blockchain);
-res.status(200).json(new ResponseModel({statusCode: 200, data: blockchain}))
+export const getBlockchain = (req, res, next) => { 
+res.status(200).json(new ResponseModel({ statusCode: 200,  data: blockchain}))
 }
 
 export const addNewBlock = (req, res, next) => {
-    const data = req.body;
+  const data = req.body;
     const lastBlock = blockchain.getLastBlock();
     const timestamp = Date.now(); 
 
@@ -26,22 +27,28 @@ export const addNewBlock = (req, res, next) => {
         data
   ); 
 
-
+ if (!block.data.data) {
+  return next(new ErrorResponse("Block must include data to be added to the blockchain", 404))
+ }
     writeToFile(folder, file, blockchain)  
   res.status(201).json(new ResponseModel({ statusCode: 201, data: block }));
 }
 
 export const blockByIndex = (req, res, next) => {
     try {
+      /* throw new Error("Something went wrong with the execution of code") */
       const blockIndex = req.params.id;
       const block = blockchain.chain[blockIndex - 1];
 
-      if (block) {
-        res.status(201).json(new ResponseModel({ statusCode: 201, data: block }));
-  
+      if(!block) {
+        return next(new ErrorResponse(`Car with id:${blockIndex} does not exist`, 404))
       }
+        res.status(201).json(new ResponseModel({
+           statusCode: 201,
+            data: block 
+          }));
     } catch (error) {
-      console.log("Could not find vehicle", error);
+      next(error)
     }
 
 
