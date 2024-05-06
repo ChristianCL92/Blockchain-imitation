8fc4b6915de5f2d3b1762442a69691447a6c6e91
Christian Lopez <chrisdev92@outlook.com>
2024-05-06 09:54:31 +0200

    added route to search for block by id

--- routes/blockchain-routes.mjs
+++ routes/blockchain-routes.mjs
@@ -1,8 +1,10 @@
 import express from "express";
-import { getBlockchain } from "../controllers/blockchain-controller.mjs";
+import { getBlockchain, addNewBlock, blockByIndex } from "../controllers/blockchain-controller.mjs";
 
 const router = express.Router();
 
 router.route("/").get(getBlockchain);
+router.route("/:id").get(blockByIndex);
+router.route("/mine").post(addNewBlock);
 
 export default router;
