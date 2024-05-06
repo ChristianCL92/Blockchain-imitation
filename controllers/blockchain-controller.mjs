6bc4e8bda97152e3114dff67ef92730959385f10
Christian Lopez <chrisdev92@outlook.com>
2024-05-06 09:35:42 +0200

    enable search for block by id

--- controllers/blockchain-controller.mjs
+++ controllers/blockchain-controller.mjs
@@ -1,24 +1,48 @@
- import ResponseModel from "../utilities/ResponseModel.mjs" 
+import ResponseModel from "../utilities/ResponseModel.mjs" 
 import blockchain from "../startBlockchain.mjs"
+  import {
+    writeToFile,
+  } from '../utilities/fileHandler.mjs';  
 
-export const getBlockchain = (req, res, next) => {
+  const folder = "data";
+const file = "blockchain.json";  
+
+
+export const getBlockchain = (req, res, next) => {  
+  console.log(blockchain);
 res.status(200).json(new ResponseModel({statusCode: 200, data: blockchain}))
 }
 
-export const createBlock = (req, res, next) => {
-  const data = req.body;
-     const previousBlockHash = 0;
-    const currentBlockHash = 0;
+export const addNewBlock = (req, res, next) => {
+    const data = req.body;
+    const lastBlock = blockchain.getLastBlock();
     const timestamp = Date.now(); 
-   const block = blockchain.createBlock(
-    timestamp,
-    previousBlockHash,
-    currentBlockHash,
-    data
+
+    const currentBlockHash = blockchain.hashBlock(timestamp, lastBlock.currentBlockHash, data);
+    const block = blockchain.createBlock(
+       timestamp,
+       lastBlock.currentBlockHash,
+       currentBlockHash,
+        data
   ); 
 
-console.log(timestamp);
 
+    writeToFile(folder, file, blockchain)  
   res.status(201).json(new ResponseModel({ statusCode: 201, data: block }));
-  console.log(block);
+}
+
+export const blockByIndex = (req, res, next) => {
+    try {
+      const blockIndex = req.params.id;
+      const block = blockchain.chain[blockIndex - 1];
+
+      if (block) {
+        res.status(201).json(new ResponseModel({ statusCode: 201, data: block }));
+  
+      }
+    } catch (error) {
+      console.log("Could not find vehicle", error);
+    }
+
+
 }
