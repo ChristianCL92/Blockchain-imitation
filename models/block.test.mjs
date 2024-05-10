2dff66384987c3121028dbf28f786be8ff613f9c
Christian Lopez <chrisdev92@outlook.com>
2024-05-10 13:49:16 +0200

    basic test for block properties

---
+++ models/block.test.mjs
@@ -0,0 +1,18 @@
+import {describe, it, expect} from "vitest";
+import Block from "./Block.mjs";
+
+describe("Block", () => {
+    
+    const block = new Block()
+    describe("Should have the following properties", () => {
+          it('Timestamp, blockIndex, previousBlockHash, currentBlockHash, data, difficulty', () => {
+            expect(block).toHaveProperty("timestamp")
+            expect(block).toHaveProperty("blockIndex")
+            expect(block).toHaveProperty("previousBlockHash")
+            expect(block).toHaveProperty("currentBlockHash")
+            expect(block).toHaveProperty("data")
+            expect(block).toHaveProperty("difficulty")
+          });
+    })
+  
+})
