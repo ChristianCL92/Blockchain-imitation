21987a1808bccfb07d564bc7975a8afa8295bd17
Christian Lopez <chrisdev92@outlook.com>
2024-05-10 13:39:03 +0200

    refactored the genesis block to this file

--- models/Block.mjs
+++ models/Block.mjs
@@ -1,10 +1,21 @@
 export default class Block {
-    constructor(timestamp, blockIndex, previousBlockHash, currentBlockHash, data, difficulty) {
-        this.timestamp = timestamp;
-        this.blockIndex = blockIndex;
-        this.previousBlockHash = previousBlockHash;
-        this.currentBlockHash = currentBlockHash;
-        this.data = data;
-        this.difficulty = difficulty;
-    }
+  constructor(
+    timestamp,
+    blockIndex,
+    previousBlockHash,
+    currentBlockHash,
+    data,
+    difficulty
+  ) {
+    this.timestamp = timestamp;
+    this.blockIndex = blockIndex;
+    this.previousBlockHash = previousBlockHash;
+    this.currentBlockHash = currentBlockHash;
+    this.data = data;
+    this.difficulty = difficulty || +process.env.DIFFICULTY;
+  }
+
+  static genesis() {
+    return new this(Date.now(), 1, '0', '0', [], process.env.DIFFICULTY);
+  }
 }
