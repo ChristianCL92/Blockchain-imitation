3b8b88d04528a87222130f2c80e3f382d06e9922
Christian Lopez <chrisdev92@outlook.com>
2024-05-12 14:30:01 +0200

    logger module

--- server.mjs
+++ server.mjs
@@ -1,10 +1,19 @@
 import express from "express";
+import { ErrorResponse } from "./utilities/ErrorResponseModel.mjs";
+import { errorHandler } from "./middleware/errorHandler.mjs";
 import blockchainRouter from "./routes/blockchain-routes.mjs"
-
+import { logger } from "./middleware/logger.mjs";
 const app = express()
 
+app.use(logger)
 app.use(express.json());
 app.use("/api/v1/blockchain", blockchainRouter);
+
+app.all("*", (req, res, next) => {
+    next(new ErrorResponse(`Could not find resource ${req.originalUrl}`, 404));
+ })
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 7010;
 
